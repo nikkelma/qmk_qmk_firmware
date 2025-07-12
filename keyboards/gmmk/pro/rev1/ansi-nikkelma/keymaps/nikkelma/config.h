538d664d56fccf55a0218e55a9625b32117d20c0
Matthew Nikkel <matt.nikkel@hey.com>
2025-07-12 10:46:28 -0700

    Change max LED brightness to 255

--- keyboards/gmmk/pro/rev1/ansi-nikkelma/keymaps/nikkelma/config.h
+++ keyboards/gmmk/pro/rev1/ansi-nikkelma/keymaps/nikkelma/config.h
@@ -17,6 +17,7 @@
 #pragma once
 
 #ifdef RGB_MATRIX_ENABLE
+    #define RGB_MATRIX_MAXIMUM_BRIGHTNESS 255
     #define RGB_DISABLE_TIMEOUT 1200000     // 20 minutes (20 * 60 * 1000ms)
     #define RGB_DISABLE_WHEN_USB_SUSPENDED
 #endif
