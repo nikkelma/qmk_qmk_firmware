b0adcbcc4d5e7753ef8ea44758bfb83f1c6c1342
Matthew Nikkel <matt.nikkel@hey.com>
2025-07-17 19:30:09 -0700

    Add missing include, rename base keyboard file

--- keyboards/gmmk/pro/rev1/ansi_nikkelma/keymaps/nikkelma/keymap.c
+++ keyboards/gmmk/pro/rev1/ansi_nikkelma/keymaps/nikkelma/keymap.c
@@ -18,6 +18,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include QMK_KEYBOARD_H
 
+#include <lib/lib8tion/lib8tion.h>
+
 typedef enum nikkelma_keycodes {
   NMD_HD8 = SAFE_RANGE,
   NMD_HD1,
